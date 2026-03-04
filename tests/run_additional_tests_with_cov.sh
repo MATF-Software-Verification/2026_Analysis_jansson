b307e44e898ea0d6c23496acba710abacc6a6cb0
Marijameme <marijanacupovic@gmail.com>
2026-03-04 11:10:17 +0100

    Add tests for dtoa.c file

---
+++ tests/run_additional_tests_with_cov.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OUTDIR="$(pwd)"
+TESTDIR="./suites"
+JANSSON_DIR="jansson"
+
+
+cd $TESTDIR
+make clean
+rm -f *.gcda *.gcno
+
+make
+
+./test_dtoa
+
+cd ../../$JANSSON_DIR
+# make distclean
+# rm -f *.gcda *.gcno
+# rm -rf src/.libs
+# ./configure CFLAGS="-g -O0 --coverage" --enable-static --disable-shared
+# make
+
+lcov --capture --directory src \
+     --gcov-tool /usr/bin/gcov \
+     --base-directory src \
+     --output-file $OUTDIR/data/new_coverage.info
+
+
+lcov --remove $OUTDIR/data/new_coverage.info \
+     '/usr/include/*' '/usr/lib/*' \
+     -o $OUTDIR/data/new_filtered.info
+
+genhtml --branch-coverage -o $OUTDIR/new_report \
+        $OUTDIR/data/new_filtered.info
